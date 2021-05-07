d32a1ecd348b58149e02cadd79e41650cef29914
Renivaldors <renivaldor@hotmail.com>
2021-05-06 22:32:37 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Renivaldors" target="_blank" rel="noreferrer">Renivaldo Rodrigues</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/renivaldo.rs" target="_blank" rel="noreferrer">@renivaldo.rs</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
